7d85af2b2055c9447f0b5f348a2c7e54e3f0b504
wenyl <761501179@qq.com>
2024-12-23 17:29:49 +0800

    add new features

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'  
-// https://vitejs.dev/config/ 
-export default defineConfig(
-    {  
-        base: './',
-    }
-)
+// import { defineConfig } from 'vite'  
+// // https://vitejs.dev/config/ 
+// export default defineConfig(
+//     {  
+//         base: './',
+//     }
+// )
